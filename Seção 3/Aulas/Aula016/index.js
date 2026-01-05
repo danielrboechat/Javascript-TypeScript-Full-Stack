5ec8ccfb238b44662eb1e97d5c0dc3c09af7e228
Daniel Boechat <dboechatt01@gmail.com>
2026-01-04 22:52:15 -0400

    Studying

---
+++ Seção 3/Aulas/Aula016/index.js
@@ -0,0 +1,26 @@
+function random(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand = 10;
+
+while (rand !== 10) {
+  console.log(rand);
+}
+console.log('######');
+
+do {
+  console.log(rand);
+} while(rand !== 10);
+
+// const nome = 'daniel'
+
+// let i = 0
+
+// while (i < nome.length) {
+//   console.log(nome[i])
+//   i++
+// } 
